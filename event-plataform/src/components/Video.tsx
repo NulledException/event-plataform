f39972d0a1f34adb6e6ea2446a955891823dfa60
NulledException <edu.cagol123@gmail.com>
2022-07-26 20:49:05 -0300

    aula-05

--- event-plataform/src/components/Video.tsx
+++ event-plataform/src/components/Video.tsx
@@ -6,43 +6,14 @@ import {
     Lightning,
 } from "phosphor-react";
 import "@vime/core/themes/default.css";
-import { gql, useQuery } from "@apollo/client";
-
-const GET_LESSON_BY_SLUG_QUERY = gql`
-    query GetLessonBySlug($slug: String) {
-        lesson(where: { slug: $slug }) {
-            title
-            videoId
-            description
-            teacher {
-                bio
-                avatarURL
-                name
-            }
-        }
-    }
-`;
-
-interface GetLessonBySlugResponse {
-    lesson: {
-        title: string;
-        videoId: string;
-        description: string;
-        teacher: {
-            bio: string;
-            avatarURL: string;
-            name: string;
-        };
-    };
-}
+import { useGetLessonBySlugQuery } from "../graphql/generated";
 
 interface VideoProps {
     lessonSlug: string;
 }
 
 export function Video(props: VideoProps) {
-    const { data } = useQuery<GetLessonBySlugResponse>(
-        GET_LESSON_BY_SLUG_QUERY,
+    const { data } = useGetLessonBySlugQuery(
         {
             variables: {
                 slug: props.lessonSlug,
@@ -50,7 +21,7 @@ export function Video(props: VideoProps) {
         }
     );
 
-    if (!data) {
+    if (!data || !data.lesson) {
         return (
             <div className="flex-1 justify-center items-center text-center h-full">
                 <p>Carregando...</p>
@@ -76,7 +47,8 @@ export function Video(props: VideoProps) {
                             <p className="mt-4 text-gray-200 leading-relaxed">
                                 {data.lesson.description}
                             </p>
-                            <div className="flex items-center gap-4 mt6">
+                            {data.lesson.teacher && (
+                                <div className="flex items-center gap-4 mt6">
                                 <img
                                     className="h-16 w-16 rounded-full border-2 border-blue-500"
                                     src={data.lesson.teacher.avatarURL}
@@ -91,6 +63,7 @@ export function Video(props: VideoProps) {
                                     </span>
                                 </div>
                             </div>
+                            )}
                         </div>
                         <div className="flex flex-col gap-4">
                             <a
